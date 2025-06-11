b05c3fab2ca44cd87a7b2cd527dd4e36828c1a93
Nindy <ninditanisd@gmail.com>
2025-06-12 00:40:45 +0700

    Create Higher Than 75 Marks.sql

---
+++ Higher Than 75 Marks.sql
@@ -0,0 +1,40 @@
+-- 01. Query the Name of any student in STUDENTS who scored higher than Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+SELECT NAME
+FROM STUDENTS
+WHERE MARKS > 75
+ORDER BY RIGHT(NAME, 3) ASC, ID ASC;
+
+
+-- 02. Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+
+
+-- 02. 
+
+
+-- 02. 
+
+
+-- 02. 
+
+
+-- 02. 
+
+
+-- 02. 
+
+
+-- 02. 
+
+
+-- 02. 
+
+
+-- 02. 
+
+
+-- 02. 
+
+
+-- 02. 
+
+
